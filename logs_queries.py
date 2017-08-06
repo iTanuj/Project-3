c99dff6f1f00da6d974b9766e7f709717bc5c453
Tanuj Sharma <tanuj.goodboy77@gmail.com>
2017-08-06 15:06:13 +0530

    updated to match PEP8 style recommendation

--- logs_queries.py
+++ logs_queries.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 import psycopg2
-
-
 DBNAME = "news"
+
 
 # executes the passed query and return it's result
 def result_of(query):
@@ -14,33 +12,38 @@ def result_of(query):
     db.close()
     return rslt
 
+
 # prints passed result formatted according to the query
-# usage: dq for double qoutes signal, sfx for apt suffix 
+# usage: dq for double qoutes signal, sfx for apt suffix
 def show(dq, result, sfx):
-	qou = '"' if dq else ''
-	for row in result:
-		print('\t{0}{1}{0} --- {2} {3}'.format(qou,row[0],row[1],sfx))
-	print()
+    qou = '"' if dq else ''
+    for row in result:
+        print('\t{0}{1}{0} --- {2} {3}'.format(qou, row[0], row[1], sfx))
+    print()
+
 
 # query for 3 most popular articles
 q1 = "select * from article_views order by views desc limit 3;"
 
+
 # query for popularity of each author in order of their article views
 q2 = '''select authors.name, sum(article_views.views) as views
-	from article_views, authors where article_views.author=authors.id
-	group by authors.name order by views desc;'''
+from article_views, authors where article_views.author=authors.id
+group by authors.name order by views desc;'''
+
 
 # query for showing dates when more than 1% requests led to errors
-q3 = "select * from err_dates where err_percent > 1.0 order by err_percent desc;"
+q3 = '''select * from err_dates where err_percent > 1.0
+order by err_percent desc;'''
 
 
 # finally calling execution and printing all queries one-by-one
 
 print("The most popular three articles of all time are:\n")
-show(True,result_of(q1), 'views')
+show(True, result_of(q1), 'views')
 
 print("The most popular article authors of all time are:\n")
-show(False,result_of(q2), 'views')
+show(False, result_of(q2), 'views')
 
 print("Days, when more than 1% of requests led to errors are:\n")
-show(False,result_of(q3), '\b% errors')
+show(False, result_of(q3), '\b% errors')
